783924270947ad49e2dc048445e536d5afa04c11
吴汉政 <hanzheng.wu@supwisdom.com>
2020-04-22 16:44:13 +0800

    更新版本号脚本

---
+++ change-version.bat
@@ -0,0 +1,15 @@
+@echo off
+set /p version=请输入版本号：
+call mvn version:set -DnewVersion=%version%
+
+if "%errorlevel%"=="1" goto :initfailed
+goto initsuccess
+
+:initfailed
+echo 更新版本号失败！
+pause
+exit
+
+:initsuccess
+echo 更新版本号成功！
+pause
